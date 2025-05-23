40710ef38b63f10bdf43d685450a6e186eb818eb
Younes0-0 <younesdemraoui@gmail.com>
2025-05-23 09:52:40 +0200

    Añadir configuración de autenticación y actualizar dependencias

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,24 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 
+import auth from 'auth-astro';
+
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    adapter: vercel(),
-    // 🧩 Añade esto: 
-    vite: {
-        resolve: {
-        alias: {
-            '@': './src',
-            '@components': '/src/components',
-            '@layouts': '/src/layouts',
-            '@pages': '/src/pages'
-        }
-        }
-    }
+  output: 'server',
+  adapter: vercel(),
+
+  // 🧩 Añade esto: 
+  vite: {
+      resolve: {
+      alias: {
+          '@': './src',
+          '@components': '/src/components',
+          '@layouts': '/src/layouts',
+          '@pages': '/src/pages'
+      }
+      }
+  },
+
+  integrations: [auth()]
 });
