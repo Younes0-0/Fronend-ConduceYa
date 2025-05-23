46aa29e871bbb5dc87c282635a509c284eb3b7e6
Younes0-0 <younesdemraoui@gmail.com>
2025-05-23 14:15:14 +0200

    Simplificar la configuración de alias en astro.config.mjs y ajustar tsconfig.json para incluir solo archivos necesarios

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
-import path from 'path';
+
 import auth from 'auth-astro';
 
 // https://astro.build/config
@@ -13,11 +13,10 @@ export default defineConfig({
   vite: {
     resolve: {
     alias: {
-        // alias          → carpeta real
-        '@':             path.resolve(__dirname, './src'),
-        '@components':   path.resolve(__dirname, './src/components'),
-        '@layouts':      path.resolve(__dirname, './src/layouts'),
-        '@pages':        path.resolve(__dirname, './src/pages'),
+        '@': './src',
+        '@components': '/src/components',
+        '@layouts': '/src/layouts',
+        '@pages': '/src/pages'
       }
     }
   },
